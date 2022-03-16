635341b84f9b66224c318264760910cd9702b4aa
Jeremy Bunting <buntingj@gmail.com>
2022-03-16 11:40:48 -0400

    stripping out header and index content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,22 +11,7 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	<a href="/tests">link to tests</a>
 </section>
 
 <style>
